d0619c34cc48bc8f97f3308ef130014ca7d8bd25
Jeremy Bell <grcodemonkey@live.com>
2013-11-27 15:53:41 -0500

    Removing unused code

--- src/360Recognition.RestClientLib/Properties/AssemblyInfo.cs
+++ src/360Recognition.RestClientLib/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("HTTP/REST Client")]
 [assembly: AssemblyCompany("Terryberry")]
 [assembly: AssemblyProduct("360Recognition.RestClient")]
-[assembly: AssemblyCopyright("Copyright © Terryberry 2013")]
+[assembly: AssemblyCopyright("Copyright Terryberry 2010 - 2014")]
 [assembly: AssemblyTrademark("Be Recognized")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("a1f0430c-7cae-4fb5-85ac-fb0e37f10780")]
 
-[assembly: AssemblyVersion("2013.11.18.0")]
+[assembly: AssemblyVersion("2013.11.27.0")]
