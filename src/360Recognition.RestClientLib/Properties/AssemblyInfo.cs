8eb2917e19aeb1803f556aec47b7a32f2d5d4953
Patrick Makarwicz <pmakarewicz@terryberry.com>
2024-04-11 12:31:45 -0400

    master: target/convert to net standard

--- src/360Recognition.RestClientLib/Properties/AssemblyInfo.cs
+++ src/360Recognition.RestClientLib/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Terryberry.Http")]
-[assembly: AssemblyDescription("HTTP/REST Client for Terryberry 360 Recognition platform")]
-[assembly: AssemblyCompany("Terryberry.Http")]
-[assembly: AssemblyProduct("Terryberry.Http")]
-[assembly: AssemblyCopyright("Copyright Terryberry 2010 - 2017")]
 [assembly: AssemblyTrademark("Be Recognized")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a1f0430c-7cae-4fb5-85ac-fb0e37f10780")]
-
-/* -------------------------------------------------------------------------------------------------
-/ Use the http://semver.org/ schema.  For pre release build add -name# for pre release versioning.
-/ See also http://docs.nuget.org/docs/reference/versioning
-/ -------------------------------------------------------------------------------------------------- */
-[assembly: AssemblyVersion("1.5.4")]
-[assembly: AssemblyInformationalVersion("1.5.4")]
